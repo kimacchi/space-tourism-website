767cd2dd48d315bf2f9bb2954ffc8cd984dcdc20
Ahmet Ergül <shiftusu2@gmail.com>
2022-02-24 13:46:54 +0300

    i f*cked up but fixed it

--- src/components/index.js
+++ src/components/index.js
@@ -9,16 +9,4 @@ export {default as Douglas} from "./crew/douglas/Douglas";
 export {default as Mark} from "./crew/mark/Mark";
 export {default as Victor} from "./crew/victor/Victor";
 
-export {default as Europa} from "./destination/europa/Europa";
-export {default as Mars} from "./destination/mars/Mars";
-export {default as Moon} from "./destination/moon/Moon";
-export {default as Titan} from "./destination/titan/Titan";
-
-export {default as CrewNavbar} from "./navbar/crewNavbar/CrewNavbar";
-export {default as DestinationNavbar} from "./navbar/destinationNavbar/DestinationNavbar";
 export {default as HeaderNavbar} from "./navbar/headerNavbar/HeaderNavbar";
-export {default as TechnologyNavbar} from "./navbar/technologyNavbar/TechnologyNavbar";
-
-export {default as Launch} from "./technology/launch/Launch";
-export {default as Spacecapsule} from "./technology/spacecapsule/Spacecapsule";
-export {default as Spaceport} from "./technology/spaceport/Spaceport";
